c46005d44b701fd8cf4b029c7daef3c40e7cd076
Tarek <elhaj102@mail.chapman.edu>
2022-05-21 01:30:42 -0700

    changed quote results to .text instead of .json

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
-	styledComponents: true
+	styledComponents: true,
+	images: {
+		domains: [ 'static.finnhub.io' ]
+	}
 };
 
 module.exports = nextConfig;
